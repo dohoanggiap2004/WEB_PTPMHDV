01ba01961cc0128a63be3eb0d6ffb3127b6e2fd4
TSongHao <hao270904@gmail.com>
2024-12-06 22:28:28 +0700

    Feat: Installment searching

--- server/serverJavaBE/src/main/java/com/commercial/app/domain/dtos/response/InstallmentResponseDto.java
+++ server/serverJavaBE/src/main/java/com/commercial/app/domain/dtos/response/InstallmentResponseDto.java
@@ -2,21 +2,30 @@ package com.commercial.app.domain.dtos.response;
 
 import jakarta.persistence.Column;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class InstallmentResponseDto {
+    private String installmentId;
 
-    private Long installmentId;
     private String company; // Company
+
     private int installmentPrice; // Installment price
+
     private int downPayment; // Down payment
+
     private String term; // Term in months
+
     private int monthlyInstallment; // Monthly installment
+
     private String flatInterestRate; // Flat interest rate
+
     private String requiredDocuments; // Required documents
+
     private int totalPayment; // Total payment
 }
