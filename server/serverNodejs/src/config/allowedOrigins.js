ff2e8e5251b4e820fe0188563d2c83b26cdd669b
dohoanggiap2004 <dohoanggiap2004@gmail.com>
2024-12-12 09:30:45 +0700

    rm js file

--- server/serverNodejs/src/config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'https://www.google.com.vn',
-    'http://127.0.0.1:5500',
-    'http://localhost:3000',
-    'http://localhost:3001'
-];
-
-module.exports = allowedOrigins
--- server/serverNodejs/src/routers/refreshToken.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const RefreshTokenController = require('../app//controllers/RefreshTokenController')
-
-router.get('/refresh-token', RefreshTokenController.handleRefreshToken)
-
-module.exports = router
